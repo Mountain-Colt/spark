bab7094cf709cd5389653e87b072cb12ecf1e19a
Will Burgo <wburgo999@gmail.com>
2014-11-10 16:33:48 -0800

    updated

--- simpleApp.py
+++ simpleApp.py
@@ -1,7 +1,7 @@
-"""SimpleApp.py"""
+"""SimpleApp.py"""  # this only works locally, will update once we have a working master deployed
 from pyspark import SparkContext
-
-logFile = "/Users/williamburgo/Desktop/spark-1.1.0-bin-hadoop2.4/README.md"  # Should be some file on your system
+sparkDirectory = "/Users/williamburgo/Desktop/spark-1.1.0-bin-hadoop2.4" # directory where spark is built
+logFile = sparkDirectory + "/README.md"  # Should be some file on your system
 sc = SparkContext("local", "Simple App")
 logData = sc.textFile(logFile).cache()
 
