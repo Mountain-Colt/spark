018b65ef9125d6cbc40d333a60af65053553a52c
Will Burgo <wburgo999@gmail.com>
2014-11-12 12:12:46 -0800

    add configuration for connecting slave to mesos master

--- simpleApp.py
+++ simpleApp.py
@@ -9,3 +9,13 @@ numAs = logData.filter(lambda s: 'a' in s).count()
 numBs = logData.filter(lambda s: 'b' in s).count()
 
 print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
+
+
+#for configuring slave to connect to a mesos master
+"""
+val conf = new SparkConf()
+  .setMaster("mesos://HOST:5050")
+  .setAppName("My app")
+  .set("spark.executor.uri", "<path to spark-1.1.0.tar.gz uploaded above>")
+val sc = new SparkContext(conf)
+""
